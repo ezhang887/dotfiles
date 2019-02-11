ea35a1e1c18fed7154377829ea1490f067bc68d5
Eric Zhang <ezhang887@gmail.com>
2019-02-11 01:40:44 -0600

    update installation script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-LOG_FILE=./dotfiles_setup.txt
+timestamp=`date '+%Y_%m_%d__%H_%M_%S'`
+LOG_FILE="./dotfiles_setup_$timestamp.txt"
 
 install_apt () {
     sudo apt-get -y install $1
-    if type -p $1 > /dev/null; then
-        echo "[setup.sh] $1 not installed!" >> $LOG_FILE
+    rv=$?
+    if [ "$rv" = 0 ]; then
+        echo "[success] [setup.sh] $1 succesfully installed" >> $LOG_FILE
     else
-        echo "[setup.sh] $1 succesfully installed" >> $LOG_FILE
+        echo "[error]   [setup.sh] $1 not installed!" >> $LOG_FILE
     fi
 }
 
-sudo apt-get update && sudo apt-get full-upgrade
+#basic apt-get installations
+sudo apt-get update #&& sudo apt-get full-upgrade
 install_apt "vim"
 install_apt "git"
 install_apt "curl"
 install_apt "git"
 install_apt "build-essential"
+install_apt "chromium-browser"
+install_apt "openjdk-8*"
+install_apt "python-pip"
+install_apt "python3-pip"
+install_apt "vlc"
+install_apt "valgrind"
+install_apt "gdb"
